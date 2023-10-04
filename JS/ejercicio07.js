cee71a7529fc1653de9b9db87e8cd861b7d8adc8
Susana Bergaz <susannabergaz@hotmail.com>
2023-10-04 13:52:50 +0200

    Ejercicio incompleto

---
+++ JS/ejercicio07.js
@@ -0,0 +1,9 @@
+//Crea una función en la que pasándole un string como parámetrose substituyan las letras A por las O.
+
+
+//Crea una función que compruebe si un string pasado como parámetro empieza por ‘aca’ y llama dos 
+//veces a la función una con academia y otra con escuela.
+
+
+//Crea una función que pasándole un Hola nos salude 3 veces utilizando métodos de Strings.
+
