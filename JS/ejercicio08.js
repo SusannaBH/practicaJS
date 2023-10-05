442b5ac0d2ae4a07a7312c53fb632c295a6c2a31
Susana Bergaz <susannabergaz@hotmail.com>
2023-10-05 12:43:02 +0200

    Ejercicios Ok

---
+++ JS/ejercicio08.js
@@ -0,0 +1,13 @@
+const cadena = "I❤️code";
+
+//BUCLE CON FOR
+for (let i = 0; i < 10; i++) {
+    console.log("FOR = " + cadena);
+}
+
+//BUCLE CON WHILE
+let num = 0;
+while (num < 10) {
+    console.log("WHILE = " + cadena);
+    num++;
+}
