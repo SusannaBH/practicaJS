e250453bc9623e9e2e5848a5370a99095ee878e5
Susana Bergaz <susannabergaz@hotmail.com>
2023-10-05 13:02:25 +0200

    Ejercicios Ok

---
+++ JS/ejercicio09.js
@@ -0,0 +1,12 @@
+//Define un array con las letras ‘a’, ‘b’, ‘c’, ‘d’ y ‘e’. Recorre cada uno de los elementos mediante a forEach
+const arr = ["a", "b", "c", "d", "e"];
+
+arr.forEach(element => {
+    console.log("foreach " + element);
+});
+
+//Define una variable let numero = 5; que itere hasta que el valor sea 0
+for (let numero = 5; numero >= 0; numero--) {
+
+    console.log("for " + numero);
+};
